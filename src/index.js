994e1666e8bc0b622a63fde48c9f28be8d29edee
James Hatfield <james.hatfield17@icloud.com>
2025-03-28 12:26:49 +0000

    Created initial functions to get weather data

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ function getWeather(location) {
         "headers": {
         }
         })
-      .then(response => {
-        console.log(response);
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        // Getting the data we need (subject to change)
+        console.log(response.currentConditions);
+        console.log(response.currentConditions.conditions);
+        console.log(response.currentConditions.temp);
+        console.log(response.currentConditions.feelslike);
       })
       .catch(err => {
         console.error(err);
@@ -15,4 +22,4 @@ function getWeather(location) {
       
 }
 
-console.log(getWeather("venice"));
+console.log(getWeather("Zurich"));
