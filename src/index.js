91f7977f9bb0196f83445875c1d06f2e2f5bb320
James Hatfield <james.hatfield17@icloud.com>
2025-03-28 12:10:43 +0000

    Add function to fetch API data, prints to console

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./styles.css";
 
+function getWeather(location) { 
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=WE5SWL7PXQVWZVJPQW6JNH35P&contentType=json`, {
+        "method": "GET",
+        "headers": {
+        }
+        })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+      
+}
+
+console.log(getWeather("venice"));
